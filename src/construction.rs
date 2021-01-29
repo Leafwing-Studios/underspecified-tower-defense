669b3efc0910296b03c9efb63f55ee467fd1d5d5
Alice Cecile <alice.i.cecile@gmail.com>
2021-01-28 21:07:12 -0500

    Added initial player resources

--- src/construction.rs
+++ src/construction.rs
@@ -2,8 +2,26 @@ use bevy::prelude::*;
 
 pub struct ConstructionPlugin;
 
-impl Plugin for ConstructionPlugin{
-	fn build(&self, app: &mut AppBuilder) {
-        // add things to your app here
+impl Plugin for ConstructionPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.init_resource::<Gold>();
+    }
+}
+
+#[derive(Default)]
+struct Gold {
+    value: u64,
+}
+
+impl Deref for Gold {
+    type Target = u64;
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+
+impl DerefMut for Gold {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.value
     }
 }
--- src/experience.rs
+++ src/experience.rs
@@ -1,9 +1,47 @@
 use bevy::prelude::*;
+use std::ops::{Deref, DerefMut};
 
 pub struct ExperiencePlugin;
 
-impl Plugin for ExperiencePlugin{
-	fn build(&self, app: &mut AppBuilder) {
-        // add things to your app here
+impl Plugin for ExperiencePlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.init_resource::<PowerXp>()
+            .init_resource::<SpecializationXp>();
+    }
+}
+
+#[derive(Default)]
+struct PowerXp {
+    xp: u64,
+}
+
+impl Deref for PowerXp {
+    type Target = u64;
+    fn deref(&self) -> &Self::Target {
+        &self.xp
+    }
+}
+
+impl DerefMut for PowerXp {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.xp
+    }
+}
+
+#[derive(Default)]
+struct SpecializationXp {
+    xp: u64,
+}
+
+impl Deref for SpecializationXp {
+    type Target = u64;
+    fn deref(&self) -> &Self::Target {
+        &self.xp
+    }
+}
+
+impl DerefMut for SpecializationXp {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.xp
     }
 }
